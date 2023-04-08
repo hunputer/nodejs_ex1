a71cad8f20763b044d70b58d6181d6ead25f37d0
hunputer <wlgns3855@gmail.com>
2023-04-08 16:33:44 +0900

    commit

--- class/01-01-token/index.js
+++ class/01-01-token/index.js
@@ -1,6 +1,10 @@
 console.log("안녕하세요")
 
 
-function getToken(){const result =  String(Math.floor(Math.random() * 1000000)).padStart(6,"0");console.log(result);}
+function getToken(aaaa)
+{
+    const result =  String(Math.floor(Math.random() * aaaa)).padStart(aaaa,"0");
+    console.log(result);
+}
 
-getToken();
+getToken(3);
