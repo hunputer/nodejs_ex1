29e56050966821de44d3438ab42efccf08e0cb1b
hunputer <wlgns3855@gmail.com>
2023-04-09 23:47:15 +0900

    commit

---
+++ class/02-01-welcome-template/index.js
@@ -0,0 +1,18 @@
+function getWelcomeTemplate(){
+    const result = `
+        <html>
+            <body>
+                <h1>철수님 가입을 환영합니다!!!</h1>
+                <hr/>
+                <div>이름 : 철수</div>
+                <div>나이 : 13</div>
+                <div>학교 : 다람쥐초등학교</div>
+                <div>가입일 : 2020-01-02</div>
+            </body>
+        </html>
+    `
+
+    console.log(result);
+}
+
+getWelcomeTemplate();
