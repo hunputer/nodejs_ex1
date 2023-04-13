d89de29e42592e6de1121d35649955564e214ddd
hunputer <wlgns3855@gmail.com>
2023-04-13 19:59:00 +0900

    commit

---
+++ class/06-01-async-await/index.js
@@ -0,0 +1,17 @@
+import axios from 'axios'
+
+//비동기방식
+// function fetchPost(){
+//     const result = axios.get("https://koreanjson.com/posts/1");
+//     console.log(result);
+// }
+
+// fetchPost();
+
+//동기방식
+async function fetchPost2(){
+    const result = await axios.get("https://koreanjson.com/posts/1");
+    console.log(result);
+}
+
+fetchPost2();
