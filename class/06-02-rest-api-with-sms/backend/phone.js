e03bdd54b337f29e734c35e7c28fec50692b51f7
hunputer <wlgns3855@gmail.com>
2023-04-14 16:24:18 +0900

    commit

--- class/06-02-rest-api-with-sms/backend/phone.js
+++ class/06-02-rest-api-with-sms/backend/phone.js
@@ -29,12 +29,15 @@ export function createToken(aaaa){
 }
 
 export async function printTokenResult(phone, token){
+    const SMS_KEY = process.env.SMS_KEY;
+    const SMS_SECRET = process.env.SMS_SECRET;
+    const SMS_SENDER = process.env.SMS_SENDER;
 
     const mysms = coolsms.default;
-    const messageService = new mysms("NCS9KXDXPCSEXXVU","FU9W4FY1XL6UQP9IQKV0GJ1NP87UNXIH");
+    const messageService = new mysms(SMS_KEY, SMS_SECRET);
     const result = await messageService.sendOne({
         to: `${phone}`,
-        from : '01072773791',
+        from : SMS_SENDER,
         text : `안녕하세요 요청하신 인증번호는 [${token}]입니다.`
     })
 
