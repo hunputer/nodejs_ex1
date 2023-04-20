81d6b5e1a7f7a6c176fa1dfaa388e5e8502f6de5
hunputer <wlgns3855@gmail.com>
2023-04-20 16:15:35 +0900

    commit

--- class/11-04-express-with-OOP/index.js
+++ class/11-04-express-with-OOP/index.js
@@ -1,4 +1,6 @@
 import express from 'express'
+import { CashService } from './cash'
+import { ProductService } from './product'
 
 const app = express()
 
@@ -6,27 +8,34 @@ const app = express()
 // 상품 구매하기
 app.post("/products/buy",(req, res) => {
     // 1. 가진돈 검증하는 코드 (대략 10줄 정도)
-    // ...
+    const cashService = new CashService()
+    const hasMoney = cashService.checkValue();
 
     // 2. 판매여부 검증하는 코드 (대략 10줄 정도)
-    // ...
-
+    const productService = new ProductService();
+    const isSoldout = productService.checkSoldout();
+    
     // 3. 상품 구매하는 코드
     // if(돈있음 && 판매완료){
 
-    res.send("상품 구매 완료!!")
+    if(hasMoney && !isSoldout){
+        res.send("상품 구매 완료!!")
+    }
 })
 
 
 // 상품 환불하기
 app.post("/products/refund",(req, res) => {
     // 1. 판매여주 검증하는 코드(대략 10줄 정도)
-    // ...
+    const productService = new ProductService();
+    const isSoldout = productService.checkSoldout();
 
 
     //2. 상품 환불하는 코드
     // if(판매완료)
-    res.send("상품 환불 완료!!")
+    if(isSoldout){
+        res.send("상품 환불 완료!!")
+    }
 })
 
 
---
+++ class/11-04-express-with-OOP/product.js
@@ -0,0 +1,6 @@
+export class ProductService{
+    checkSoldout = () =>{
+        // 2. 판매여부 검증하는 코드 (대략 10줄 정도)
+        // ...
+    }
+}
