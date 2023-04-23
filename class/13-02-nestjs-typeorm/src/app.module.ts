9d2215c34c37e55fcdf69c33f0dc622c71b6264f
hunputer <wlgns3855@gmail.com>
2023-04-24 01:02:45 +0900

    commit

--- class/13-02-nestjs-typeorm/src/app.module.ts
+++ class/13-02-nestjs-typeorm/src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { BoardModule } from './apis/boards/boards.module';
 import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { Board } from './apis/boards/entities/board.entity';
 
 @Module({
   imports: [
@@ -9,6 +11,17 @@ import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: 'src/commons/graphql/schema.gql',
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: '127.0.0.1',
+      port: 3306,
+      username: 'root',
+      password: '1234',
+      database: 'myproject03',
+      entities: [Board],
+      synchronize: true,
+      logging: true,
     }),
   ],
 })
